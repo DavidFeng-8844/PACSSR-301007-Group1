81770b759d18d1614acc72779d15b856238e2fac
David Feng <110443929+DavidFeng-8844@users.noreply.github.com>
2025-03-22 00:01:53 +0800

    a

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ cd dist
 git init
 git add -A
 git commit -m "deploy"
-git push -f https://github.com/DavidFeng-8844/PACSSR-301007-Group5.git main:gh-pages
+git push -f https://github.com/DavidFeng-8844/PACSSR-301007-Group1.git main:gh-pages
 cd ..
